c7fcad8e731642a0a971a85437ab97da478140b5
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-25 16:55:12 -0500

    Switch to Halite for easier handling of an HTTP Client

--- src/github-cr/client.cr
+++ src/github-cr/client.cr
@@ -1,4 +1,5 @@
-require "http/client"
+# require "http/client"
+require "halite"
 require "./models/user"
 require "./errors"
 require "json"
@@ -7,25 +8,22 @@ module Github
   class Client
     getter user
 
-    def initialize(@username : String, @password : String, user_agent = "github-cr/#{Github::VERSION}", timeout = 15)
-      @http_client = HTTP::Client.new(URI.parse(Github::BASE_API_URL))
-      @http_client.basic_auth(@username, @password)
-      @http_client.connect_timeout = timeout
+    def initialize(username : String, password : String, user_agent = "github-cr/#{Github::VERSION}",
+                   timeout = 15, max_redirects = 5)
+      # Initialize Halite HTTP Client for all subsequent requests
+      @http_client = Halite::Client.new do
+        basic_auth username, password
+        headers accept: "application/vnd.github.v3+json"
+        headers user_agent: user_agent
+        timeout timeout
+        follow max_redirects
+      end
 
-      http_headers = HTTP::Headers{
-        "Accept"     => "application/vnd.github.v3+json",
-        "User-Agent" => user_agent,
-      }
-
-      client_response = @http_client.get("/user", http_headers)
+      client_response = @http_client.get("#{Github::BASE_API_URL}/user")
 
       raise Github::HTTPError.new(401, "The user was not authenticated!") if client_response.status_code == 401
 
       @user = Github::User.from_json(client_response.body)
-    end
-
-    def finalize
-      @http_client.close
     end
   end
 end
