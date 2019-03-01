2b4bcae7a1a5a314d85ca0ebfe5242dc012f0a6d
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-01 18:12:47 -0500

    Use JSON helper in client spec

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -5,12 +5,10 @@ describe GithubCr::Client do
     context "when user is authenticated" do
       context "when login isn't passed" do
         it "returns the client user" do
-          json_body = File.read("#{__DIR__}/mocked_json/client_user.json")
-
           WebMock.stub(:get, "https://api.github.com/user")
             .with(headers: {"Accept"     => "application/vnd.github.v3+json",
                             "User-Agent" => "github-cr/#{GithubCr::VERSION}"})
-            .to_return(status: 200, body: json_body)
+            .to_return(status: 200, body: get_json("client_user.json"))
 
           client = GithubCr::Client.new("octocat", "octocat_password")
 
@@ -23,12 +21,10 @@ describe GithubCr::Client do
 
       context "when login is passed" do
         it "returns the corresponding Github user" do
-          json_body = File.read("#{__DIR__}/mocked_json/other_user.json")
-
           WebMock.stub(:get, "https://api.github.com/users/octocat")
             .with(headers: {"Accept"     => "application/vnd.github.v3+json",
                             "User-Agent" => "github-cr/#{GithubCr::VERSION}"})
-            .to_return(status: 200, body: json_body)
+            .to_return(status: 200, body: get_json("other_user.json"))
           client = GithubCr::Client.new("octocat", "octocat_password")
 
           octocat_user = client.user("octocat")
@@ -57,12 +53,10 @@ describe GithubCr::Client do
         end
 
         it "raises a GithubCr::HTTPError with 401" do
-          json_body = File.read("#{__DIR__}/mocked_json/mocked_401.json")
-
           WebMock.stub(:get, "https://api.github.com/user")
             .with(headers: {"Accept"     => "application/vnd.github.v3+json",
                             "User-Agent" => "github-cr/#{GithubCr::VERSION}"})
-            .to_return(status: 401, body: json_body)
+            .to_return(status: 401, body: get_json("mocked_401.json"))
 
           client = GithubCr::Client.new("octocat", "octocat_password")
 
