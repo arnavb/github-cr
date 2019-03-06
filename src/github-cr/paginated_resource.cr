4f70156cbcbaad20cba43f54d84880158db58ca6
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-06 16:32:26 -0500

    Implement basic working pagination

--- src/github-cr/paginated_resource.cr
+++ src/github-cr/paginated_resource.cr
@@ -1,34 +1,48 @@
 require "http/client"
 require "http/client/response"
 
+require "json"
+
 module GithubCr
+  class Page(T)
+    getter json_data : Array(JSON::Any)
+
+    def initialize(json_string : String, @http_client : HTTP::Client, @http_headers : HTTP::Headers, @complete_request = false)
+      @json_data = JSON.parse(json_string).as_a
+    end
+
+    def each
+      @json_data.each do |object|
+        if @complete_request
+          url = object["url"].as_s
+          response = @http_client.get(url, @http_headers)
+          GithubCr.handle_http_errors(response) unless response.success?
+          yield T.new(response.body, @http_client, @http_headers)
+        else
+          yield T.new(object.as_h.to_json, @http_client, @http_headers)
+        end
+      end
+    end
+  end
+
   class PaginatedResource(T)
     getter current_response
 
     def initialize(initial_response : HTTP::Client::Response,
-                   @http_client : HTTP::Client, @http_headers : HTTP::Headers)
+                   @http_client : HTTP::Client, @http_headers : HTTP::Headers, @complete_request = false)
       @current_response = initial_response
-    end
-
-    def next
-      link_headers = PaginatedResource.parse_link_headers(@current_response.headers["Link"])
-
-      if link_headers.has_key?("next")
-        response = @http_client.get(link_headers["next"], @http_headers)
-        GithubCr.handle_http_errors(response) unless response.success?
-        @current_response = response
-        T.new(@current_response.body, @http_client, @http_headers)
-      end
     end
 
     def each
       link_headers = PaginatedResource.parse_link_headers(@current_response.headers["Link"])
 
-      while link_headers.has_key?("next")
+      loop do
+        yield Page(T).new(@current_response.body, @http_client, @http_headers, @complete_request)
+        break unless link_headers.has_key?("next")
         response = @http_client.get(link_headers["next"], @http_headers)
         GithubCr.handle_http_errors(response) unless response.success?
         @current_response = response
-        yield T.new(@current_response.body, @http_client, @http_headers)
+        link_headers = PaginatedResource.parse_link_headers(@current_response.headers["Link"])
       end
     end
 
